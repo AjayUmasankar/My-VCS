2502b0508e6274bbc3b065f9b5afa16bd8c3ba78
Ajay Umasankar <z5025214@unsw.edu.au>
2018-10-01 07:13:24 +1000

    give on Mon  1 Oct 07:13:24 AEST 2018

--- legit.pl
+++ legit.pl
@@ -2,7 +2,6 @@
 
 
 sub main() {
-
 	if ($ARGV[0] eq "init") {
 		if (!-d ".legit") {
 			mkdir ".legit";
@@ -18,18 +17,109 @@ sub main() {
 		add_files(@ARGV[1..$#ARGV]);
 	} elsif ($ARGV[0] eq "commit") {
 		#my $message = $ARGV[2];
-		commit_index();
-		
-		# Empty out index!!
+		if ($ARGV[1] eq "-m") {
+			commit_index($ARGV[2]);	# only -m option
+		} else {
+			update_index();
+			commit_index($ARGV[3]);	# -a option..
+		}
 	} elsif ($ARGV[0] eq "log") {
 		show_log();
 	} elsif ($ARGV[0] eq "show") {
 		$ARGV[1] =~ /([0-9]*):(.*)/;
 		show_commit($1, $2);
+	} elsif ($ARGV[0] eq "rm") {
+		my $indexOnly = 0;
+		my $force = 0;
+		my $startIndex;
+		if ($ARGV[1] ne "--force" && $ARGV[1] ne "--cached") { # two arguments
+			$startIndex = 1;
+		} elsif ($ARGV[1] eq "--force" && $ARGV[2] eq "--cached") { # four arguments
+			$force = 1;
+			$indexOnly = 1;
+			$startIndex = 3;
+		} elsif ($ARGV[1] eq "--cached" && $ARGV[2] eq "--force") {
+			$force = 1;
+			$indexOnly = 1;
+			$startIndex = 3;
+		} elsif ($ARGV[1] eq "--force") {   #three arguments (rm, --(force|cached), filenames)
+			$force = 1;
+			$startIndex = 2;
+		} else {
+			$indexOnly = 1;
+			$startIndex = 2;
+		}
+		remove_file($indexOnly, $force, @ARGV[$startIndex..$#ARGV]);
+	}
+}
+#sub file_exists { 
+#	my ($file1) = @_;
+#	open FILE1, '<', $file1 or return 0;
+#	close FILE1;
+#	#open FILE2, '<', $file2 or return 0;
+#	#close FILE2;
+#	return 1;
+#}
+
+sub remove_file {
+	my ($indexOnly, $force, @files) = @_;
+	my $oldSnapshot = get_last_snapshot();
+	foreach my $fileName (@files) {
+		my $indexFile = ".legit/index/$fileName";
+		my $repositoryFile = ".legit/$oldSnapshot/$fileName";
+		my $currentFile = "$fileName";
+
+
+	if ($force != 1) {	
+		if (-e $repositoryFile && -e $indexFile && -e $currentFile) {
+			if (!same_file($indexFile, $repositoryFile) && 
+				!same_file($indexFile, $currentFile)) {
+				die "legit.pl: error: '$fileName' in index is different to both working file and repository\n";
+			}
+		}
+		
+		if (-e $indexFile && $indexOnly == 0) {
+			if (! -e $repositoryFile) {
+				die "legit.pl: error: '$fileName' has changes staged in the index\n";
+			} elsif (-e $repositoryFile) {
+				if (!same_file($indexFile, $repositoryFile)) {
+					die "legit.pl: error: '$fileName' has changes staged in the index\n";
+				}
+			}
+			
+#			if (! -e $currentFile) {
+#				die "legit.pl: error: '$indexFile' has changes staged in the index\n";
+#			}
+		}
+
+		if (-e $repositoryFile && -e $currentFile && $indexOnly == 0) {
+			if (!same_file($repositoryFile, $currentFile)) {
+				die "legit.pl: error: '$fileName' in repository is different to working file\n";
+			}
+		}
+	}
+		if (! -e $indexFile) {
+			die "legit.pl: error: '$fileName' is not in the legit repository\n";		
+		}
+		
+		if (! -e $currentFile && $indexOnly == 0) {
+			die "$currentFile doesnt exist :)\n";
+		}
+	
+		# Deleting index files first
+		unlink $indexFile;
+		if ($indexOnly == 0) {
+			unlink $currentFile;
+		}
+	
 	}
 }
 
-sub show_commit() {
+#sub delete_file {
+	#my ($file1, $file2, $force) = @_;
+	#if ($force == 1) {
+
+sub show_commit() {   #commitID:fileName 
 	my ($commitID, $fileName) = @_;
 	my $folder;
 	if ($commitID !~ /[0-9]+/) {
@@ -45,8 +135,8 @@ sub show_commit() {
 	print <$file>;
 }
 
-sub update_log() {
-	my $message = $ARGV[2];
+sub update_log {    #logs are updated after successful commits
+	my ($message) = @_;
 	#print "$message is message\n";
 	open my $log, '>>', ".legit/log.txt" or die;
 	$numCommits = log_lines();
@@ -56,14 +146,14 @@ sub update_log() {
 	close $log;
 }
 
-sub log_lines() {
+sub log_lines() {     #how many lines/commits there are in the log
 	open my $log, '<', ".legit/log.txt" or die;
 	@lines = <$log>;
 	close $log;
 	return @lines;
 }
 
-sub show_log() {	
+sub show_log() {      #prints the contents of the log
 	#test log
 	open my $log, '<', ".legit/log.txt" or die;
 	my @logLines = <$log>;
@@ -73,47 +163,75 @@ sub show_log() {
 	#print "is updated log\n";
 	close $log;
 }
-
-sub commit_index() {
-	my ($indexDir) = ".legit/index";
-	my $file;
-	my $oldSnapshot = get_last_snapshot();
-	my $newSnapshot = get_new_snapshot(); #doesnt create, just gets the next possible directory
-	$newSnapshot =~ /\.snapshot\.(.*)/;
-	$commitNum = $1;
-
-	#Makes new directory if necessary
-	foreach $file (glob "$indexDir/*") {	#file contains path relative to current directory
+sub update_index() {  #used to update index for the -a tag in commit
+	my $indexDir = ".legit/index";
+	foreach $file (glob "$indexDir/*") {
 		my $fileName = $file;
-		$fileName =~ s/.*\///;	# gets the name of the file being copied 
-		if (!same_file("$file", ".legit/$oldSnapshot/$fileName")) {
-			if (! -d ".legit/$newSnapshot") {
-				mkdir ".legit/$newSnapshot";
+		$fileName =~ s/.*\///;
+		if (!same_file("$file", "$fileName")) {
+			unlink $file;
+			copy_file("$fileName", "$file");
+#			print "Replaced $file\n";
+		}
+	}
+}
+	
+sub commit_index() {  #commits by creating a new snapshot and transferring files from index
+	my ($message) = @_;
+	my $indexDir = ".legit/index";
+	my $oldSnapshot = get_last_snapshot();
+	my $indexHasChanged = 0;
+
+	# Checks if all files in index exist in old snapshot
+	foreach my $indexFile (glob "$indexDir/*") {	#file contains path relative to current directory
+		my $fileName = $indexFile;
+		$fileName =~ s/.*\///;
+		my $oldRepoFile = ".legit/$oldSnapshot/$fileName";
+		if (! -e "$oldRepoFile") { 				#if index has changed from last 
+			$indexHasChanged = 1;				#snapshot, make new dir
+		} elsif (-e "$oldRepoFile") { 				#changed = different no. of files OR
+			if (!same_file("$indexFile", "$oldRepoFile")) {	#same file with different contents
+				$indexHasChanged = 1;
 			}
-			#copy_file("$file", ".legit/$newSnapshot/$fileName");
 		}
 	}
-	if (! -d ".legit/$newSnapshot") { 
-		die "nothing to commit\n"; 
-	} else {	
-		#the directory does exist(so atleast one change from oldSnapshot 
-		#  so we need to copy everything from index
-		foreach $file (glob "$indexDir/*") {
-			my $fileName = $file;
-			$fileName =~ s/.*\///;
-			copy_file("$file", ".legit/$newSnapshot/$fileName");
+
+	# Checks if all files in old snapshot exist in index
+	foreach my $oldRepoFile (glob ".legit/$oldSnapshot/*") {
+		my $fileName = $oldRepoFile;
+		$fileName =~ s/.*\///;
+		if (! -e "$indexDir/$fileName") {
+			$indexHasChanged = 1;
 		}
 	}
+	
+	# Creates new repo/snapshot and then commits all files in index to it
+	
+	if ($indexHasChanged == 0) {
+		die "nothing to commit\n";
+	}
+
+	my $newSnapshot = get_new_snapshot();
+	$newSnapshot =~ /\.snapshot\.(.*)/;
+	my $commitNum = $1;
+	mkdir ".legit/$newSnapshot";
+	foreach my $indexFile (glob "$indexDir/*") {
+		my $fileName = $indexFile;
+		$fileName =~ s/.*\///;
+		copy_file("$indexFile", ".legit/$newSnapshot/$fileName");
+	}
+
 	print "Committed as commit $commitNum\n"; 
-	update_log();
+	update_log($message);
 	#return 1;
 }
 
+# Returns 0 if they are not the same file(or if one of the files dont exist), 1 if they are
 sub same_file() {
 	my ($file1, $file2) = @_;
-	open FILE1, '<', $file1 or die;		# if the file doesnt exist in oldSnapshot(FILE2),  
+	open FILE1, '<', $file1 or die;	# if the file doesnt exist in oldSnapshot(FILE2),  
 					        # new commit is required. Return 0
-	open FILE2, '<', $file2 or return 0;
+	open FILE2, '<', $file2 or die;
 	my @lines1 = <FILE1>;
 	my @lines2 = <FILE2>;
 	close FILE1;
@@ -171,18 +289,24 @@ sub get_new_snapshot { # Copied from Lab08 Sample Solution
 }
 
 sub add_files {
-        #$indexDir = "
+        $indexDir = ".legit/index/";
         my (@files) = @_;
 	my $file;
 	foreach $file (@files) {
-		if (! -e $file) {
-			print "legit.pl: error: can not open 'non_existent_file'\n";
-			exit 1;
+		if (! -e $file && ! -e "$indexDir$file") {
+			die "legit.pl: error: can not open 'non_existent_file'\n";
+		} elsif (! -e $file && -e "$indexDir$file") {
+			#wierd subset 0_13 case 
+			unlink "$indexDir$file";
+		} else {
+			copy_file("$file", "$indexDir$file")
 		}
 	}
-        foreach $file (@files) {
-                copy_file($file,".legit/index/$file");
-        }
+        #foreach $file (@files) {
+	#	if (-e $file) {
+        #       	copy_file($file,".legit/index/$file");
+	#	}
+        #}
 }
 
 
